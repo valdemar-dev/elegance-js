1d2c45a41a0b5f3b0452368880262be7ce55a5af
valdemar <valdemar@github.noreply.com>
2025-02-17 16:21:04 +0200

    watcher no longer resets state on soft reload

--- src/client/watcher.ts
+++ src/client/watcher.ts
@@ -28,8 +28,6 @@ if (Object.values(pd)[0]?.w) {
             // Adding dates to href's on links makes them re-fetch.
             const href = link.getAttribute('href')!;
             link.setAttribute('href', href.split('?')[0] + '?' + new Date().getTime());
-
-            loadPage();
         } else if (event.data === "hard-reload") {
             window.location.reload();
         }
