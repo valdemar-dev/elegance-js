c1eab8a37272a67533cc76134857b617d681edc7
valdemar-dev <graveyarddev@proton.me>
2025-10-25 23:06:09 +0300

    prod mode

--- docs/.elegance/dist/routing/page_data.js
+++ docs/.elegance/dist/routing/page_data.js
@@ -1,13 +1,1 @@
-let url = "/routing";
-export const data = { state: [{ id: 37, value: ["/pages/recipes/cake/page.ts"] }, { id: 38, value: function anonymous(state, event) {
-  (async (_, codeContent2, toastContent2) => {
-    await navigator.clipboard.writeText(`${codeContent2.value}`);
-    toastContent2.value = "Copied to Clipboard";
-    toastContent2.signal();
-  })(event, ...state.getAll([{ id: 37 }, { id: 6 }]));
-} }], soa: [{ "id": 38, "key": 29, "attribute": "onclick" }] };
-if (!globalThis.pd) {
-  globalThis.pd = {};
-}
-;
-globalThis.pd[url] = data;
+let o="/routing";export const data={state:[{id:37,value:["/pages/recipes/cake/page.ts"]},{id:38,value:function(i,l){(async(d,e,a)=>{await navigator.clipboard.writeText(`${e.value}`),a.value="Copied to Clipboard",a.signal()})(l,...i.getAll([{id:37},{id:6}]))}}],soa:[{id:38,key:29,attribute:"onclick"}]};globalThis.pd||(globalThis.pd={}),globalThis.pd[o]=data;
