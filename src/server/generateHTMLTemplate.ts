9dfe66b66c2c8d30bdc0c3bb69fcd54bdc95a472
valdemar-dev <graveyarddev@proton.me>
2025-10-27 21:06:53 +0200

    I hate path construction.

--- src/server/generateHTMLTemplate.ts
+++ src/server/generateHTMLTemplate.ts
@@ -33,7 +33,7 @@ export const generateHTMLTemplate = async ({
     if (addPageScriptTag === true) {
         const sanitized = pageURL === "" ? "/" : `/${pageURL}`;
         
-        StartTemplate += `<script data-page="true" type="module" data-pathname="${sanitized}" src="${sanitized}/${name}_data.js" defer="true"></script>`;
+        StartTemplate += `<script data-page="true" type="module" data-pathname="${sanitized}" src="${sanitized.endsWith("/") ? sanitized : sanitized + "/"}${name}_data.js" defer="true"></script>`;
     }
 
     StartTemplate += `<script type="module" src="/client.js" defer="true"></script>`;
