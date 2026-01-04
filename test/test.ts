34ec1a480e55696fde6e44dc45a9a724ee70cb50
valdemar-dev <graveyarddev@proton.me>
2026-01-04 22:09:59 +0200

    add test.ts

---
+++ test/test.ts
@@ -0,0 +1,5 @@
+/**
+ * Modify this to run whatever basic usage of Elegance you want to test.
+ * It's not actual testing, it's more like sanity-testing.
+ */
+import { compile } from "../src/build";
