c15114872cda14f74713c292521ac1d0b6910ee9
valdemar-dev <graveyarddev@proton.me>
2026-01-06 16:45:43 +0200

    not working, but still working on it

--- test/test.ts
+++ test/test.ts
@@ -3,35 +3,13 @@
  * It's not actual testing, it's more like sanity-testing.
  */
 import { allElements, } from "../src/elements/element_list";
-import { serializeElement, } from "../src/compilation/compiler";
-import { EleganceElement, ProcessSpecialElementOption, SpecialElementOption } from "../src/elements/element";
+import { compileEntireProject, compileStaticPageToDisk, generatePageCompilationContext, generatePageDataScript, serializeElement, setCompilerOptions, } from "../src/compilation/compiler";
 
-Object.assign(globalThis, allElements);
+(async () => {
+    setCompilerOptions({
+        pagesDirectory: "./test_pages",
+        outputDirectory: ".elegance",
+    });
 
-class EventListener extends SpecialElementOption {
-    value: { callback: () => void, dependencies: any[], };
-
-    constructor(callback: () => void, dependencies: any[]) {
-        super();
-
-        this.value = { callback, dependencies };
-    }
-
-    serialize(element: EleganceElement<any>, optionName: string) {
-        delete element.options[optionName];
-
-        return { clientToken: this.value, }
-    }
-}
-
-function eventListener(callback: () => void, dependencies: any[]) {
-    return new EventListener(callback, dependencies);
-}
-
-const onClick = eventListener(() => {}, []);
-
-const h1 = allElements["h1"];
-
-const el = h1({ "onClick": onClick, });
-
-console.log(serializeElement(el))
+    await compileEntireProject();
+})();
---
+++ test/test_pages/layout.ts
@@ -0,0 +1,8 @@
+export const layout = (child: any) => {
+    return html();
+};
+
+export const metadata = () => {
+    return head();
+};
+export const isDynamic = false;
